6122cb88f73350a4901bb704af688fe62d2b8d83
Aviral Srivastav <59115616+kokaviral@users.noreply.github.com>
2021-11-09 21:46:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", function (req, res) {
   res.render("home.ejs");
 });
 
-var port = process.env.PORT||3000;
+var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Server Started!!");
 });
